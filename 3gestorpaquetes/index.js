33d555f9c4c1f82cc8674cc2ec2fa08a92ada3d2
JohanHdezRy <129459165+JohanHdezRy@users.noreply.github.com>
2025-02-20 17:08:06 -0500

    Add files via upload

---
+++ 3gestorpaquetes/index.js
@@ -0,0 +1,10 @@
+/*
+var generateName = require('sillyname');
+var sillyName = generateName();
+
+console.log(sillyName);
+*/
+
+import {randomSuperhero} from 'superheroes';
+
+console.log(randomSuperhero());
