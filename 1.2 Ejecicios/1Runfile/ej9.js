c7fa75244d9a9b0fb1e028a910d7cec15792ab3a
JohanHdezRy <129459165+JohanHdezRy@users.noreply.github.com>
2025-02-25 17:00:58 -0500

    Add files via upload

---
+++ 1.2 Ejecicios/1Runfile/ej9.js
@@ -0,0 +1,14 @@
+//sumar 
+function sumar(a, b) {
+    return a + b;
+  }
+//restar 
+function restar(a, b) {
+    return a - b;
+  }
+// multiplicar 
+ function multiplicar(a, b) {
+    return a * b;
+  }
+  
+module.exports = { sumar, restar, multiplicar };
